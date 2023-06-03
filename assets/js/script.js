63a668fd7319efab4f1d3ebab54f8919badf8683
Paola <brito.moreau@gmail.com>
2023-06-02 23:08:58 -0400

    Creación método cargarTarjeta. Término del ejercicio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,52 @@
 //5. variable global
 
 let salmon = document.getElementById("numeroSalmon");
-let apple = document.getElementById("numeroSalmon");
-let blue = document.getElementById("numeroSalmon");
+let apple = document.getElementById("numeroApple");
+let blue = document.getElementById("numeroBlue");
 
 
 
-//1.clase de personaje
+//1.clase de personaje 4. se creo método crearTarjetas
 
 class Personaje{
-    constructor(nombre, estatura, peso){
+    constructor(nombre, estatura, peso, row){
         this.nombre = nombre
         this.estatura = estatura
         this.peso = peso
+        this.row = row
     }
+
+    cargarTarjeta = (color)=>{
+
+        document.getElementById(`${this.row}`).innerHTML += `
+        <div class="col-12 col-md-6 col-lg-4 ">
+            <div class="single-timeline-content d-flex wow fadeInLeft alturaTarjeta" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
+                <div class="timeline-icon ${color}"></div>
+                <div class="timeline-text">
+                    <h6>${this.nombre}</h6>
+                    <p>Estatura: ${this.estatura}cm.  </p>
+                    <p>Peso: ${this.peso} kg.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+        `
+    
+    }
+
 
 }
 
 //2. consumo de API for fetch
 
-const traerPersonaje = async (id, color)=> {
+/* const URL_BASE = `https://swapi.dev/api/people/` */
+
+const traerPersonaje = async (id, color, row)=> {
     try{
-        let result = await fetch(`http https://swapi.dev/api/people/${id}`)
-        let personaje = await result.json()
-        let algo = crearPersonaje(personaje);
-        cargarTarjeta (algo, color);
+        let result = await fetch(`https://swapi.dev/api/people/${id}`);
+        let respuesta = await result.json();
+        let personaje = crearPersonaje(respuesta, row);
+        personaje.cargarTarjeta(color);
         
     }catch (error){
         throw new Error (error)
@@ -34,64 +56,46 @@ const traerPersonaje = async (id, color)=> {
 
 //3. función crear objeto personaje
 
-const crearPersonaje = (personaje)=>{
-    let personaje = new Personaje(personaje.name, personaje.height, personaje.mass);
-    return personaje
+const crearPersonaje = (personaje, row)=>{
+    let infoPersonaje = new Personaje(personaje.name, personaje.height, personaje.mass, row);
+    return infoPersonaje
 }
 
 
-
-//4. crear tarjeta
-
-const cargarTarjeta = (traerPersonaje, color)=>{
-    let html = ` 
-        <div class="row">
-        <div class="col-12 col-md-6 col-lg-4">
-            <div class="single-timeline-content d-flex wow fadeInLeft" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInLeft;">
-                <div class="timeline-icon ${color}"></div>
-                <div class="timeline-text">
-                    <h6>${traerPersonaje.name}}</h6>
-                    <p>Estatura: ${traerPersonaje.height}cm. Peso: ${traerPersonaje.mass} kg. </p>
-                </div>
-            </div>
-        </div>
-    </div>
-        ` 
-        return html
-}
 
 //5. Funcion generadora con 3 parámetos
-function * generador(id, color){
-    yield traerPersonaje(id, color);
+function * generador(id, color, row){
+    yield traerPersonaje(id, color, row);
     id++
-    yield traerPersonaje(id, color);
+    yield traerPersonaje(id, color, row);
     id++
-    yield traerPersonaje(id, color);
+    yield traerPersonaje(id, color, row);
     id++
-    yield traerPersonaje(id, color);
+    yield traerPersonaje(id, color, row);
     id++
-    yield traerPersonaje(id, color);
+    yield traerPersonaje(id, color, row);
     id++
 
 }
 
-let gen1 = generador(1, "salmon");
-let gen2 = generador(6, "apple");
-let gen3 = generador(12, "blue");
+let gen1 = generador(1, "salmon", "rowSalmon");
+let gen2 = generador(6, "apple", "rowApple");
+let gen3 = generador(12, "blue", "rowBlue");
 
 //6. 
 
-salmon.addEventListener("mouseenter", () => {
+salmon.addEventListener("click", async () => {
     let data = gen1.next()
-    data.done ? data.value : alert("no hay más personajes");
+    !data.done ? data.value : alert("no hay más personajes");
 }) 
     
-apple.addEventListener("mouseenter", () => {
-    let data = gen1.next()
-    data.done ? data.value : alert("no hay más personajes");
+apple.addEventListener("click", async() => {
+    let data = gen2.next()
+    !data.done ? data.value : alert("no hay más personajes");
 }) 
-blue.addEventListener("mouseenter", () => {
-    let data = gen1.next()
-    data.done ? data.value : alert("no hay más personajes");
+blue.addEventListener("click", async() => {
+    let data = gen3.next()
+    !data.done ? data.value : alert("no hay más personajes");
 }) 
+
 
